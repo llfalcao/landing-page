182019498dad4d06851e567873dfd9337fba2e86
Luiz Falcão <39093175+llfalcao@users.noreply.github.com>
2024-05-25 01:49:13 -0300

    Add success message on sign up form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,28 +2,33 @@ import { FormEvent, useState } from "react";
 
 export default function SignupForm() {
   const [email, setEmail] = useState("");
+  const [subscribed, setSubscribed] = useState(false);
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     localStorage.setItem("user", email);
-    window.location.reload();
+    setSubscribed(true);
   };
 
   return (
     <div className="container">
-      <form className="newsletter" onSubmit={handleSubmit}>
-        <label htmlFor="email">Sign up for our latest deals!</label>
-        <br />
-        <input
-          id="email"
-          type="email"
-          name="email"
-          placeholder="myemail@email.com"
-          value={email}
-          onChange={({ target }) => setEmail(target.value)}
-        />
-        <input type="submit" id="submit-email" value="Submit" />
-      </form>
+      {subscribed ? (
+        <p>Thank you for subscribing!</p>
+      ) : (
+        <form className="newsletter" onSubmit={handleSubmit}>
+          <label htmlFor="email">Sign up for our latest deals!</label>
+          <br />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="my@email.com"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          />
+          <input type="submit" id="submit-email" value="Submit" />
+        </form>
+      )}
     </div>
   );
 }
