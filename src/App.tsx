deaafe0b7c905543185330b607cc30e1fc497fbd
Luiz Falcão <39093175+llfalcao@users.noreply.github.com>
2024-05-25 00:58:51 -0300

    Migrate to Vite and Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.scss";
+// @ts-ignore
 import HomeSectionOne from "./components/sections/HomeSectionOne";
+// @ts-ignore
 import HomeSectionTwo from "./components/sections/HomeSectionTwo";
 
 function App() {
-    return (
-        <div className="App">
-            {HomeSectionOne}
-            {HomeSectionTwo}
-        </div>
-    );
+  return (
+    <div className="App">
+      {HomeSectionOne}
+      {HomeSectionTwo}
+    </div>
+  );
 }
 
 export default App;
